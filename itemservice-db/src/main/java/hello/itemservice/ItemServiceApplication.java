7a4420d9025bcb52d1cf92a6a7abca10fd12facb
SeoYangHun <syh8088@docfriends.com>
2022-06-05 20:40:30 +0900

    데이터 접근 기술 - Querydsl, 데이터 접근 기술 - 활용 방안
    데이터 접근 기술 - Querydsl, 데이터 접근 기술 - 활용 방안

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,8 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JpaConfig;
-import hello.itemservice.config.MyBatisConfig;
-import hello.itemservice.config.SpringDataJpaConfig;
+import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +16,9 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV3Config.class)
 // //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+//@Import(QueryDslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
