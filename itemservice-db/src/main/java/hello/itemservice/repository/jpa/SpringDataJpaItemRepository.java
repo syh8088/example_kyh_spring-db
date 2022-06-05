875ce513e7b0a5059430fafbfc1da0a7312d7bbe
SeoYangHun <syh8088@docfriends.com>
2022-06-05 09:30:11 +0900

    데이터 접근 기술 - 스프링 데이터 JPA
    데이터 접근 기술 - 스프링 데이터 JPA

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,20 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    //쿼리 메서드 기능
+    List<Item> findByItemNameLike(String itemName);
+
+    //쿼리 직접 실행
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+
+    List<Item> findByPriceLessThanEqual(Integer maxPrice);
+}
